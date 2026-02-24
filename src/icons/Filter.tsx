0f549caba723ca2b2de0476b75f9e6f5fbfbbed8
savenok <magnolia.sav@gmail.com>
2026-02-24 09:40:08 +0300

    upd

--- src/icons/Filter.tsx
+++ src/icons/Filter.tsx
@@ -3,15 +3,15 @@ import React from 'react';
 const Filter = () => {
     return (
         <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-            <line x1="20" y1="15" x2="20" y2="30" stroke="#fff" stroke-width="3" stroke-linecap="square"/>
-            <line x1="20" y1="48" x2="20" y2="85" stroke="#fff" stroke-width="3" stroke-linecap="square"/>
-            <line x1="52" y1="15" x2="52" y2="55" stroke="#fff" stroke-width="3" stroke-linecap="square"/>
-            <line x1="52" y1="73" x2="52" y2="85" stroke="#fff" stroke-width="3" stroke-linecap="square"/>
-            <line x1="80" y1="15" x2="80" y2="25" stroke="#fff" stroke-width="3" stroke-linecap="square"/>
-            <line x1="80" y1="43" x2="80" y2="85" stroke="#fff" stroke-width="3" stroke-linecap="square"/>
-            <rect x="7" y="30" width="26" height="18" fill="none" stroke="#fff" stroke-width="3"/>
-            <rect x="39" y="55" width="26" height="18" fill="none" stroke="#fff" stroke-width="3"/>
-            <rect x="68" y="25" width="26" height="18" fill="none" stroke="#fff" stroke-width="3"/>
+            <line x1="20" y1="15" x2="20" y2="30" stroke="#1D1D1F" stroke-width="5" stroke-linecap="square"/>
+            <line x1="20" y1="48" x2="20" y2="85" stroke="#1D1D1F" stroke-width="5" stroke-linecap="square"/>
+            <line x1="52" y1="15" x2="52" y2="55" stroke="#1D1D1F" stroke-width="5" stroke-linecap="square"/>
+            <line x1="52" y1="73" x2="52" y2="85" stroke="#1D1D1F" stroke-width="5" stroke-linecap="square"/>
+            <line x1="80" y1="15" x2="80" y2="25" stroke="#1D1D1F" stroke-width="5" stroke-linecap="square"/>
+            <line x1="80" y1="43" x2="80" y2="85" stroke="#1D1D1F" stroke-width="5" stroke-linecap="square"/>
+            <rect x="7" y="30" width="26" height="18" fill="none" stroke="#1D1D1F" stroke-width="5"/>
+            <rect x="39" y="55" width="26" height="18" fill="none" stroke="#1D1D1F" stroke-width="5"/>
+            <rect x="68" y="25" width="26" height="18" fill="none" stroke="#1D1D1F" stroke-width="5"/>
         </svg>
     );
 };
